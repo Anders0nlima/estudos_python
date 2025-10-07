cf2ae4c7222c07fd80890bcb073c30d8ad6139c0
Anders0nlima <andersoncon85@gmail.com>
2025-10-07 13:55:12 -0300

    .

--- UEPA/listaHardcore/lista1/questao5.py
+++ UEPA/listaHardcore/lista1/questao5.py
@@ -13,3 +13,33 @@ Entrada: "Olá, olá! Mundo... mundo?"
 Saída: 2 ('olá' e 'mundo') 
 
 """
+
+
+import string
+
+def frase_unica(frase):
+    frase = frase.lower()
+
+    for pontuacao in string.punctuation:
+        frase = frase.replace(pontuacao, "")
+
+    palavras = frase.split()
+
+    unicas = []
+
+
+    for palavra in palavras:
+        if palavra not in unicas:
+            unicas.append(palavra)
+    
+    return len(unicas), unicas
+
+frase = "Olá, olá! Mundo... mundo?"
+quantidade, lista_unicas = frase_unica(frase)
+
+print("Palavras únicas:", lista_unicas)
+print("Quantidade:", quantidade)
+
+
+
+
