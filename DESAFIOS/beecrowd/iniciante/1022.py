96f09cda79668e1d04cb39314671def3193efded
Anders0nlima <andersoncon85@gmail.com>
2026-08-23 09:08:12 -0300

    bee8

---
+++ DESAFIOS/beecrowd/iniciante/1022.py
@@ -0,0 +1,31 @@
+A, B, C = input().split()
+
+
+A = float(A)
+B = float(B)
+C = float(C)
+
+lista = [A, B, C]
+lista.sort(reverse=True)
+
+A = lista[0]
+B = lista[1]
+C = lista[2]
+
+descLista = [A, B, C]
+
+
+if A >= B + C:
+    print("NAO FORMA TRIANGULO")
+else:
+    if A**2 == B**2 + C**2:
+        print("TRIANGULO RETANGULO")
+    elif A**2 > B**2 + C**2:
+        print("TRIANGULO OBTUSANGULO")
+    elif A**2 < B**2 + C**2:
+        print("TRIANGULO ACUTANGULO")
+
+    if A == B == C:
+        print("TRIANGULO EQUILATERO")
+    elif A == B or B == C or C == A:
+        print("TRIANGULO ISOSCELES")
